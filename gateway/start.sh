482229aac95f83cd36fac0fb49d1a5ef00dce188
GhisLaine Laios <laios54@proton.me>
2022-07-24 01:01:18 +0800

    Update.

--- gateway/start.sh
+++ gateway/start.sh
@@ -3,7 +3,6 @@
 set -e
 BASEDIR=$(dirname $(readlink -f "$0"))
 cd $BASEDIR && \
-
 java -Xms512M -Xmx512M \
  -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions \
  -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch \
