33339da5bc2a290baa86653575a0d0bf0417535d
GhisLaine Laios <laios54@proton.me>
2022-07-24 11:56:30 +0800

    Update.

--- init.sql
+++ init.sql
@@ -3,13 +3,13 @@ create schema if not exists 'minecraft-logger';
 create schema if not exists 'minecraft-logger-velocity';
 create schema if not exists 'minecraft-luckperms';
 
-create user 'minecraft-logger' identified with 'caching_sha2_password' by random password;
+create user if not exists 'minecraft-logger' identified with 'caching_sha2_password' by random password;
 
 grant all privileges on `minecraft-logger`.* to 'minecraft-logger';
 
 grant all privileges on `minecraft-logger-velocity`.* to 'minecraft-logger';
 
-create user 'minecraft-luckperms' identified with 'caching_sha2_password' by random password;
+create user if not exists 'minecraft-luckperms' identified with 'caching_sha2_password' by random password;
 
 grant all privileges on `minecraft-luckperms`.* to 'minecraft-luckperms';
 commit;
